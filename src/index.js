3c8f2b1aebaf78f4b03b40ff91fdb8ba736a8d76
Stanislau Yakubouski <ystl1302@gmail.com>
2022-09-07 20:41:18 +0400

    feat greeting, getUserName, getRandomNumber, gameLogicMaker in the index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = () => console.log('Welcome to the Brain Games!');
+
+export const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+  return userName;
+};
+
+export const getRandomNumber = (max = 100) => Math.round((Math.random() * max));
+
+export const gameLogicMaker = (gameInfo) => {
+  console.log(gameInfo.rules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const question = gameInfo.gameQuestions[i];
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = gameInfo.gameAnswers[i];
+    if (correctAnswer !== userAnswer) {
+      const wrongAnswer = console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let's try again, ${gameInfo.userName}!`);
+
+      return wrongAnswer;
+    }
+    console.log('Correct!');
+  }
+  const congratulations = console.log(`Congratulations, ${gameInfo.userName}!`);
+
+  return congratulations;
+};
