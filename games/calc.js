5ca00fd80b252ac56e23b3403fb595b245fb7702
Stanislau Yakubouski <ystl1302@gmail.com>
2022-09-07 20:44:13 +0400

    add calc.js and feat calcGame

---
+++ games/calc.js
@@ -0,0 +1,43 @@
+import {
+  greeting, getUserName, getRandomNumber, gameLogicMaker,
+} from '../src/index.js';
+
+const calcGame = () => {
+  const getCalcGameInfo = () => {
+    const operators = ['-', '+', '*'];
+    const gameQuestions = [];
+    const gameAnswers = [];
+
+    for (let i = 0; i < 3; i += 1) {
+      const num1 = getRandomNumber();
+      const operator = operators[getRandomNumber(2)];
+      const num2 = getRandomNumber();
+
+      const question = `${num1} ${operator} ${num2}`;
+      gameQuestions.push(question);
+
+      let answer;
+      if (operator === '-') {
+        answer = num1 - num2;
+      } else if (operator === '+') {
+        answer = num1 + num2;
+      } else {
+        answer = num1 * num2;
+      }
+      gameAnswers.push(String(answer));
+    }
+
+    return { gameQuestions, gameAnswers };
+  };
+
+  const gameInfo = getCalcGameInfo();
+  gameInfo.rules = 'What is the result of the expression?';
+
+  greeting();
+
+  gameInfo.userName = getUserName();
+
+  gameLogicMaker(gameInfo);
+};
+
+export default calcGame;
