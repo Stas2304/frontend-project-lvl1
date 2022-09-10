eb2bae7ce2e4d5ee97d17320e7860408bab2f692
Stanislau Yakubouski <ystl1302@gmail.com>
2022-09-10 04:41:14 +0400

    fix bug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export const greeting = () => console.log('Welcome to the Brain Games!');
 
 export const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
 
