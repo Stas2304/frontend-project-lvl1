e203bb3d4119d3e5d929eb8ede44ed626b3d2465
Stanislau Yakubouski <ystl1302@gmail.com>
2022-10-04 20:52:55 +0400

    fix bugs

--- src/games/prime.js
+++ src/games/prime.js
@@ -13,7 +13,7 @@ const isPrimeNumber = (number) => {
 const getPrimeGameInfo = () => {
   const gameQuestions = [];
   const gameAnswers = [];
-  const gameRules = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+  const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   for (let i = 0; i < roundsCount; i += 1) {
     const numberForGame = getRandomNumber();
